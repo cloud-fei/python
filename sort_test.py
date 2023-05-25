b857b6c0ede2d18ed8692ced524289a2765ed78f
cloud-fei <983539538@qq.com>
2023-05-25 15:57:58 +0800

    sort function

---
+++ sort_test.py
@@ -0,0 +1,16 @@
+id_list = [2,1,3,5,4]
+#print(id_list)
+
+id_list.sort()
+#print(id_list)
+
+id_list.sort(reverse=True)
+#print(id_list)
+
+
+'''
+[2, 1, 3, 5, 4]
+[1, 2, 3, 4, 5]
+[5, 4, 3, 2, 1]
+'''
+
